e42bb192c38f45112b86fe2d043ff0d60691a75c
Pradeepg06 <pradeepg.cs24@bmsce.ac.in>
2024-10-20 18:24:25 +0530

    student.java

--- student.java
+++ student.java
@@ -1,83 +1,54 @@
 import java.util.Scanner;
 
-public class Student {
-    private String usn;
-    private String name;
-    private int[] credits;
-    private int[] marks;
-    private int numSubjects;
+class Stud_details {
+    int marks[] = new int[3];
+    String usn, name;
+    Scanner sc = new Scanner(System.in);
+    double sgpa;
 
-    // Constructor
-    public Student(int numSubjects) {
-        this.numSubjects = numSubjects;
-        this.credits = new int[numSubjects];
-        this.marks = new int[numSubjects];
-    }
-
-    // Method to accept student details
-    public void acceptDetails() {
-        Scanner scanner = new Scanner(System.in);
-
+    void getdetails() {
         System.out.print("Enter USN: ");
-        usn = scanner.nextLine();
-
+        usn = sc.nextLine();
         System.out.print("Enter Name: ");
-        name = scanner.nextLine();
-
-        for (int i = 0; i < numSubjects; i++) {
-            System.out.print("Enter credits for subject " + (i + 1) + ": ");
-            credits[i] = scanner.nextInt();
-
+        name = sc.nextLine();
+        for (int i = 0; i < 3; i++) {
             System.out.print("Enter marks for subject " + (i + 1) + ": ");
-            marks[i] = scanner.nextInt();
+            marks[i] = sc.nextInt();
         }
     }
 
-    // Method to display student details
-    public void displayDetails() {
+    void display() {
         System.out.println("\nStudent Details:");
         System.out.println("USN: " + usn);
         System.out.println("Name: " + name);
-
-        for (int i = 0; i < numSubjects; i++) {
-            System.out.println("Subject " + (i + 1) + ": Credits = " + credits[i] + ", Marks = " + marks[i]);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Marks of subject " + (i + 1) + ": " + marks[i]);
         }
+        System.out.println("SGPA: " + calculateSGPA());
     }
 
-    // Method to calculate SGPA
-    public double calculateSGPA() {
-        double totalCredits = 0;
-        double totalGradePoints = 0;
-
-        for (int i = 0; i < numSubjects; i++) {
-            totalCredits += credits[i];
-
-            // Convert marks to grade points (assuming 10-point scale)
-            double gradePoints = 0;
-            if (marks[i] >= 90) gradePoints = 10;
-            else if (marks[i] >= 80) gradePoints = 9;
-            else if (marks[i] >= 70) gradePoints = 8;
-            else if (marks[i] >= 60) gradePoints = 7;
-            else if (marks[i] >= 50) gradePoints = 6;
-            else gradePoints = 0;
-
-            totalGradePoints += credits[i] * gradePoints;
+    double calculateSGPA() {
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
         }
-
-        return totalGradePoints / totalCredits;
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter number of subjects: ");
-        int numSubjects = scanner.nextInt();
-
-        Student student = new Student(numSubjects);
-        student.acceptDetails();
-        student.displayDetails();
-
-        double sgpa = student.calculateSGPA();
-        System.out.println("\nSGPA: " + sgpa);
+        sgpa = (double) totalMarks / 3;
+        return sgpa;
     }
 }
+
+ class Student { // Note the 'public' access modifier
+    public static void main(String[] args) {
+        Stud_details s1[] = new Stud_details[3];
+        for (int j = 0; j < 3; j++) {
+            s1[j] = new Stud_details();
+        }
+        for (int j = 0; j < 3; j++) {
+            System.out.println("Enter the marks of " + (j + 1) + " student details");
+            s1[j].getdetails();
+        }
+        for (int j = 0; j < 3; j++) {
+            s1[j].display();
+        }
+    }
+ }
